57e47c4e18fbe5fc47ea0dd80874c15e839ac927
Jesse Lu <mr.jesselu@gmail.com>
2012-01-04 13:02:27 -0800

    Nearly patient.

--- __init__.py
+++ __init__.py
@@ -1,12 +1,16 @@
 from marionette.units import Unit
+import math
+import random 
 class Toad(Unit):
     def act(self):
+
         # Eat if we can
         if self.inventory.get('grutonium', 0) > 0:
             return 'eat', {
                 'typename': 'grutonium',
                 'amount': self.inventory['grutonium']
             }
+
         # Grab an item if we can
         items = self.items_in_view
         for item in items:
@@ -15,13 +19,29 @@ class Toad(Unit):
                     'uid': item.uid,
                     'amount': item.amount
                 }
+
         # Move to an item if we see one (not necessarily closest)
         if len(items) > 0:
-            target = items[0]
-            return 'move', {
+           target = get_closest_item(self.pos, items)
+           return 'move', {
                 'pos': target.pos
             }
-        # Move to (0, 0) if there's nothing better to do
+
+
+        # Move "randomly"
+        step = 100
         return 'move', {
-            'pos': (0, 0)
+            'pos': self.pos 
+#             'pos': (self.pos[0] + step * math.cos(self.health/30), \
+#                 self.pos[1] + step * math.sin(self.health/30))
         }
+
+def get_closest_item(pos, items):
+    mindist = 1e9
+    for item in items:
+        dist = math.hypot(item.pos[0]-pos[0],\
+            item.pos[1]-pos[1])
+        if dist < mindist:
+            target = item
+            mindist = dist
+    return target
